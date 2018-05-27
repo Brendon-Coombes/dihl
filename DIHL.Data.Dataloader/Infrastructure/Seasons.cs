ad9adb096403ab9dbc534dee2c9092ed2e2190b1
Brendon Coombes <Brendon.Coombes@intergen.co.nz>
2018-05-27 22:03:10 +1200

    Create page for schedule page, and create navigation to it that waits for it to load.

---
+++ DIHL.Data.Dataloader/Infrastructure/Seasons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIHL.Data.Dataloader.Infrastructure
+{
+    public enum Season
+    {
+        WinterDIHL2016 = 26993,
+        WinterDIHL2017 = 52559,
+        WinterDIHL2018 = 63446,
+        TheFarm2018 = 63447,
+    }
+}
