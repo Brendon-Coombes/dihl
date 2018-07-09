79b38d09f2ac196479860eeba15f19e7f8cc2ea0
Brendon Coombes <Brendon.Coombes@intergen.co.nz>
2018-07-09 12:00:46 +1200

    Add data models for shootouts

---
+++ DIHL.Repository.Sql/Models/GameShootoutStatisticDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DIHL.Repository.Sql.Models
+{
+    public class GameShootoutStatisticDataModel : IDataModel
+    {
+        /// <summary>
+        /// The unique identifier for this game shootout statistic
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The game Id
+        /// </summary>
+        public Guid GameId { get; set; }       
+    }
+}
---
+++ DIHL.Repository.Sql/Models/SkaterShootoutStatisticDataModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DIHL.Repository.Sql.Models
+{
+    public class SkaterShootoutStatisticDataModel : IDataModel
+    {
+        /// <summary>
+        /// The unique identifier for this skater shootout statistic
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The game shooutout statistic Id
+        /// </summary>
+        public Guid GameShootoutStatisticId { get; set; } 
+
+        /// <summary>
+        /// The team Id
+        /// </summary>
+        public Guid TeamId { get; set; }
+
+        /// <summary>
+        /// The Player Id
+        /// </summary>
+        public Guid? PlayerId { get; set; }
+
+        /// <summary>
+        /// The shot number for this team
+        /// </summary>
+        public int ShotNumber { get; set; }
+
+        /// <summary>
+        /// Whether or not the shot was successful
+        /// </summary>
+        public bool Successful { get; set; }
+    }
+}
